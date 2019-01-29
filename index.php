7540d15db76d435f543a2d371d9fa98a77b14250
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-01-29 15:02:33 +0100

    Added donate link to plugin row meta

--- index.php
+++ index.php
@@ -43,3 +43,19 @@ function randomPicures_display_random_pictures($atts, $content = NULL) {
 }
         
 add_shortcode('randomPicture', 'randomPicures_display_random_pictures');
+
+
+// Plugin row meta
+
+function randomPicures_plugin_row_meta( $links, $file ) {    
+    if ( plugin_basename( __FILE__ ) == $file ) {
+        $row_meta = array(
+          'donate' => '<a href="https://www.paypal.me/prstuhlmann/2" style="color: green" target="_blank">Donate</a>'
+        );
+ 
+        return array_merge( $links, $row_meta );
+    }
+    return (array) $links;
+}
+
+add_filter( 'plugin_row_meta', 'randomPicures_plugin_row_meta', 10, 2 );
