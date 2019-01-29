9922ccb74b287238aa8e1751de1a74817a1d4eee
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-01-29 13:53:39 +0100

    Added the main function which displays random images and added the shortcode and some attributes

--- index.php
+++ index.php
@@ -20,3 +20,26 @@ add_action( 'wp_enqueue_scripts', 'randomPicures_enqueue_scripts' );
 // Allows the integration of shortcodes in widget areas:
 
 add_filter( 'widget_text', 'do_shortcode' );
+
+
+// Display random pictures
+
+function randomPicures_display_random_pictures($atts, $content = NULL) {
+    $atts = shortcode_atts(
+        [
+            'width' => '450',
+            'height' => '300',
+            'tags' => 'landscapes',
+            'color' => '',
+        ], $atts, 'randomPictures' );
+        
+    $query = new WP_Query( $atts );
+    
+    $output = '<img src="https://loremflickr.com/'.esc_attr($atts['color']).'/'.esc_attr($atts['width']).'/'.esc_attr($atts['height']).'/'.esc_attr($atts['tags']).'/all">';
+    
+    wp_reset_query();
+
+    return $output;
+}
+        
+add_shortcode('randomPicture', 'randomPicures_display_random_pictures');
